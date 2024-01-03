7c4d7914e4ebc3d2028e1e98d3c884e6faa50020
khe <khe@apac.freewheel.com>
2024-01-03 09:38:17 +0800

    .:

--- src/main/java/com/kun/MaxWheelProfit_1599.java
+++ src/main/java/com/kun/MaxWheelProfit_1599.java
@@ -10,7 +10,9 @@ public class MaxWheelProfit_1599 {
     public static void main(String[] args) {
         MaxWheelProfit_1599 maxWheelProfit1599 = new MaxWheelProfit_1599();
         int[] customers = new int[]{10, 9, 6};
-        System.out.println(maxWheelProfit1599.minOperationsMaxProfit(customers, 6, 4));
+        //System.out.println(maxWheelProfit1599.minOperationsMaxProfit(customers, 6, 4));
+        customers = new int[]{0,43,37,9,23,35,18,7,45,3,8,24,1,6,37,2,38,15,1,14,39,27,4,25,27,33,43,8,44,30,38,40,20,5,17,27,43,11,6,2,30,49,30,25,32,3,18,23,45,43,30,14,41,17,42,42,44,38,18,26,32,48,37,5,37,21,2,9,48,48,40,45,25,30,49,41,4,48,40,29,23,17,7,5,44,23,43,9,35,26,44,3,26,16,31,11,9,4,28,49,43,39,9,39,37,7,6,7,16,1,30,2,4,43,23,16,39,5,30,23,39,29,31,26,35,15,5,11,45,44,45,43,4,24,40,7,36,10,10,18,6,20,13,11,20,3,32,49,34,41,13,11,3,13,0,13,44,48,43,23,12,23,2};
+        System.out.println(maxWheelProfit1599.minOperationsMaxProfit(customers, 43, 54));
     }
 
     public int minOperationsMaxProfit(int[] customers, int boardingCost, int runningCost) {
@@ -25,6 +27,8 @@ public class MaxWheelProfit_1599 {
         int peopleInWheel = 0;
         int waitingPeople = 0;
         int interval = 0;
+        int rollings = 0;
+        int rollingForMaxProfile = 0;
 
         while (interval <customers.length || waitingPeople != 0) {
             if(interval <customers.length) {
@@ -37,11 +41,15 @@ public class MaxWheelProfit_1599 {
             int profileIfStopHere = income - cost;
             if (profileIfStopHere > maxProfit) {
                 maxProfit = profileIfStopHere;
+                rollingForMaxProfile = rollings;
             }
 
+            //It seems that this is not a valid assumption
+            /*
             if (waitingPeople == 0 && peopleInWheel == 0) {
                 continue;
             } else {
+             */
                 if (waitingPeople != 0) {
                     int onboardPeople = 0;
                     if (waitingPeople >= 4) {
@@ -59,14 +67,16 @@ public class MaxWheelProfit_1599 {
                 peopleInWheel -= jumpOff;
                 cost += runningCost;
 
+                rollings++;
                 profileIfStopHere = income - cost;
                 if (profileIfStopHere > maxProfit) {
                     maxProfit = profileIfStopHere;
+                    rollingForMaxProfile = rollings;
                 }
 
-                //System.out.println(Arrays.toString(wheel.toArray()) + "," +income + "," + cost);
-            }
+                System.out.println("For rolling " + rollings + "," + Arrays.toString(wheel.toArray()) + "," +income + "," + cost + "," + profileIfStopHere);
+          //  }
         }
-        return maxProfit>0? maxProfit:-1;
+        return maxProfit>0? rollingForMaxProfile:-1;
     }
 }
